06b3609291c07052fcb0945b9bc8791159afa735
Mick <95253947+SOVFREN@users.noreply.github.com>
2025-03-04 09:50:14 +0000

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,39 @@
+<?php
+
+$config = new stdClass();
+
+$config->app_name='Grupo Chat';
+$config->app_version='3.7';
+
+$config->site_url="http://localhost/grupo/";
+$config->force_url=false;
+$config->force_https=false;
+$config->developer_mode = false;
+$config->csrf_token=true;
+
+$config->samesite_cookies='None';
+$config->http_only_cookies=false;
+$config->cookie_domain="";
+$config->timezone='Asia/Kolkata';
+$config->file_seperator='-gr-';
+
+$config->pro_version = 'pro';
+$db_error_mode=PDO::ERRMODE_SILENT;
+
+if ($config->developer_mode) {
+    $db_error_mode=PDO::ERRMODE_EXCEPTION;
+}
+
+$config->database=[
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'bacefbae_chatsovbase',
+    'username' => 'bacefbae_dev',
+    'password' => 'Ax51Guj1988=',
+    'port' => '3306',
+    'prefix' => 'gr_',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+    'logging' => false,
+    'error' => $db_error_mode,
+];
